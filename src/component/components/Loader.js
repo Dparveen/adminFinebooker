d13b7225d5960ccc478f40c6489502c41d1332c9
Dparveen <104548308+Dparveen@users.noreply.github.com>
2024-02-16 12:23:16 +0530

    nwe

--- src/component/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loader() {
-  return (
-    <div className='text-center'>
-    <img src="https://file.mockplus.com/image/2018/04/81010689-081f-4d4c-bbbc-f2040e98e994.gif" class="img-responsive" style={{width:'60%'}}></img>
-</div>
-  )
-}
--- src/component/components/Notification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Notification() {
-  return (
-    <nav className="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
-    <marquee style={{ color: 'red', fontSize: '3em' }}>Test</marquee>
-            </nav>
-  )
-}
